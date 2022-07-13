d9c44e9053267324d7772cfcc4b14ef8e862a8b3
villaricadianne <91996994+villaricadianne@users.noreply.github.com>
2022-07-13 08:53:42 -0700

    Add Calculator UI

---
+++ script.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = function (op, a, b) {
+  add(a, b);
+  subtract(a, b);
+  multiply(a, b);
+  divide(a, b);
+};
